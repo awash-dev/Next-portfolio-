cf40a0de35ce60ed23534934259a77f087b8c73d
Mohammed Hussen <99973399+awash-dev@users.noreply.github.com>
2024-11-29 22:28:32 +0300

    Update Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -8,11 +8,13 @@ import {
 } from "react-icons/ri";
 const Socials = () => {
   return <div className='flex z-10 items-center gap-x-5 text-[20px]'>
-    <Link className='hover:text-accent transition-all duration-300' href=''> <RiYoutubeLine/> </Link>
-    <Link className='hover:text-accent transition-all duration-300' href=''> <RiInstagramLine/> </Link>
-    <Link className='hover:text-accent transition-all duration-300' href=''> <RiFacebookBoxLine/> </Link>
-    <Link className='hover:text-accent transition-all duration-300' href=''> <RiLinkedinLine/> </Link>
-    <Link className='hover:text-accent transition-all duration-300' href=''> <RiTwitterLine/> </Link>
+    <Link className='hover:text-accent transition-all duration-300' href='https://youtube.com/@awash-dev?si=IFhA92EqTA7bHOnf'> <RiYoutubeLine/> </Link>
+    <Link className='hover:text-accent transition-all duration-300' href='https://www.instagram.com/m0h4mm3d_hu553n/profilecard/?igsh=MWc5N3k4eXEwZnF3ZA=='> <RiInstagramLine/> </Link>
+    <Link className='hover:text-accent transition-all duration-300' href='
+https://www.facebook.com/mgdstudio4242?mibextid=rS40aB7S9Ucbxw6v'> <RiFacebookBoxLine/> </Link>
+    <Link className='hover:text-accent transition-all duration-300' href='https://linkedin.com/comm/mynetwork/discovery-see-all?usecase=PEOPLE_FOLLOWS&followMember=mohammed-hussen-mohammed-70981b299'> <RiLinkedinLine/> </Link>
+    <Link className='hover:text-accent transition-all duration-300' href='
+https://www.facebook.com/mgdstudio4242?mibextid=rS40aB7S9Ucbxw6v'> <RiTwitterLine/> </Link>
   </div>;
 };
 
