7b15e7c4bf3ef1a1ce585c26d28c17edcd338e4e
Mohammed Hussen <99973399+awash-dev@users.noreply.github.com>
2025-05-25 23:15:36 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,35 @@
-import "../styles/globals.css"; 
+import "../styles/globals.css";
 import Transition from "../components/Transition";
-
 import Layout from "../components/Layout";
 // next router js
 import { useRouter } from "next/router";
 // framer Motion
- 
 import { AnimatePresence, motion } from "framer-motion";
+// Import Head from next/head
+import Head from "next/head";
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
-    <html lang="en">
-            <head>
-                <title>MOhammed Portfolio's </title>
-                <meta name="description" content="Mohammed Portfolio Application" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta charSet="utf-8" />
-                <link rel="icon" href="/avatar.png" />
-            </head>
-            <body >
-       <Layout>
-      <AnimatePresence mode="wait">
-        <motion.div key={router.route} className="h-full">
-          <Transition />
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </Layout>
-            </body>
-        </html>
+    <> {/* Use a React Fragment instead of <html> */}
+      <Head>
+        {/* Global meta tags and favicon */}
+        <title>Mohammed Portfolio's</title>
+        <meta name="description" content="Mohammed Portfolio Application" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/avatar.png" />
+      </Head>
 
-   
+      <Layout>
+        <AnimatePresence mode="wait">
+          <motion.div key={router.route} className="h-full">
+            <Transition />
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </> // Close the React Fragment
   );
 }
 
