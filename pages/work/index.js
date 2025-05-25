845fb2352e30a383567b61a40cda7c5d12e8a115
Mohammed Hussen <99973399+awash-dev@users.noreply.github.com>
2025-05-25 23:40:37 +0300

    update

--- pages/work/index.js
+++ pages/work/index.js
@@ -6,35 +6,34 @@ import { fadeIn } from "../../variants";
 
 const Work = () => {
   return (
-    <div className="h-full bg-primary/30 py-36 flex items-center ">
+    <div className="h-full bg-primary/30 py-36 flex items-center justify-center"> {/* Added justify-center */}
       <Circles />
-      <div className="container mx-auto xl:mt-36 flext items-center justify-center flex-col">
+      <div className="container mx-auto h-full flex items-center justify-center flex-col"> {/* Added h-full and justify-center */}
         <div className="flex flex-col xl:flex-row gap-x-8">
-          <div className="text-center flex xl:w-[30vw] flex-col lg;text mb-4 xl:mb-0">
-            <div>
-              <motion.h2
-                variants={fadeIn("up", 0.4)}
-                initial="hidden"
-                animate="show"
-                exit="hidden"
-                className="text-3xl "
-              >
-                My Works
-              </motion.h2>
-              <motion.p
-                variants={fadeIn("down", 0.4)}
-                initial="hidden"
-                animate="show"
-                exit="hidden"
-                className="mb-4 max-w-[400px] mx-auto lg:mx-0 xl:mt-10"
-              >
-                A MERN stack workspace is ideal for creating a personal
-                portfolio website, showcasing your skills and projects.
-              </motion.p>
-            </div>
-
-            {/* Slider */}
+          {/* Text Section */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0"> {/* Added lg:text-left for text alignment */}
+            <motion.h2
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="text-3xl "
+            >
+              My Works
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("down", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0 xl:mt-10"
+            >
+              A MERN stack workspace is ideal for creating a personal
+              portfolio website, showcasing your skills and projects.
+            </motion.p>
           </div>
+
+          {/* Slider */}
           <motion.div
             variants={fadeIn("up", 0.4)}
             initial="hidden"
