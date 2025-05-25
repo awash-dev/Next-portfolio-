d72b95b6f84adb6252f6754fd7fee2dc539e1c8b
Mohammed Hussen <99973399+awash-dev@users.noreply.github.com>
2025-05-25 23:35:36 +0300

    update

--- pages/work/index.js
+++ pages/work/index.js
@@ -8,7 +8,7 @@ const Work = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center ">
       <Circles />
-      <div className="container mx-auto xl:mt-36">
+      <div className="container mx-auto xl:mt-36 flext items-center justify-center flex-col">
         <div className="flex flex-col xl:flex-row gap-x-8">
           <div className="text-center flex xl:w-[30vw] flex-col lg;text mb-4 xl:mb-0">
             <div>
